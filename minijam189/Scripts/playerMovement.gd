49ca21413e9dde5e5bb9f1e9f919d60b31233fa0
arrowira <41136800+arrowira@users.noreply.github.com>
2025-07-19 01:18:57 -0700

    slipping

--- minijam189/Scripts/playerMovement.gd
+++ minijam189/Scripts/playerMovement.gd
@@ -1,19 +1,18 @@
 extends CharacterBody2D
 
-
+var defaultSpeed = 800.0
 var SPEED = 800.0
 const JUMP_VELOCITY = -400.0
 var jumping = false
 var input_direction = Vector2.ZERO
-func _on_body_entered(body):
-	print(body.name)
+var slipping = false
 func _physics_process(delta: float) -> void:
 	if velocity.x<0:
 		$Sprite2D.flip_h = true
 	else:
 		$Sprite2D.flip_h = false
 	velocity = input_direction * SPEED
-	if !jumping:
+	if !jumping and !slipping:
 		if velocity.length() > 0.1:
 			$anim.play("walk")
 		else:
@@ -27,12 +26,26 @@ func _physics_process(delta: float) -> void:
 	else:
 		if !jumping:
 			input_direction = Input.get_vector("right","left", "up", "down")
-	if jumping == true:
-		get_node("FeetCollider").disabled = true
-	else:
-		get_node("FeetCollider").disabled = false
-
+	
 
 func _on_jump_timer_timeout() -> void:
 	SPEED/=1.5
 	jumping = false
+
+
+
+
+func _on_player_area_area_entered(area: Area2D) -> void:
+	if area.name == "bigHand" and !jumping or area.name == "littleHand" and !jumping:
+		SPEED = 0
+		
+		slipping = true
+		$anim.play("slip")
+		$slipTimer.start()
+		
+
+
+func _on_slip_timer_timeout() -> void:
+	slipping = false
+	SPEED = defaultSpeed
+	$anim.play("idle")
