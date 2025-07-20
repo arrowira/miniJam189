b549534c61523a3711bcc97af06cca5763296f10
galaxyraider9wastaken <75186182+galaxyraider9wastaken@users.noreply.github.com>
2025-07-19 17:44:20 -0700

    Hafl Working

--- minijam189/Scenes/CauldronManager.gd
+++ minijam189/Scenes/CauldronManager.gd
@@ -2,15 +2,22 @@ extends Node2D
 
 var Going = false
 var items = []
+var ari = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	StartRecipe(5)
 	pass
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	print(get_path())
 	pass
+
+func Filled(id):
+	if(items[0] == id):
+		ari+=1
+		get_node(str(ari-1)).queue_free()
+	pass
+
 #cauldron coords -21 31
 func StartRecipe(amt):
 	var d = -21-(160*(amt/2))
@@ -23,16 +30,22 @@ func StartRecipe(amt):
 		match n:
 			1:
 				Texture = "res://sprites/Eyeball.png"
+				items.append(1)
 			2:
 				Texture = "res://sprites/WashingMachineHeart.png"
+				items.append(2)
 			3:
 				Texture = "res://sprites/StrangeLump.png"
+				items.append(3)
 			4:
 				Texture = "res://sprites/CandyCorn.png"
+				items.append(4)
 			5:
 				Texture = "res://sprites/VoidEssence.png"
+				items.append(5)
 		var sprite = Sprite2D.new()
 		sprite.texture = load(Texture)
+		sprite.name = str(i)
 		sprite.scale.x = 17
 		sprite.scale.y = 17
 		sprite.position.x = d
@@ -40,6 +53,5 @@ func StartRecipe(amt):
 		sprite.position.y = -300
 		sprite.texture_filter = sprite.TEXTURE_FILTER_NEAREST
 		get_node(get_path()).add_child(sprite)
-		pass
 		
 	pass
--- minijam189/Scripts/cauldron.gd
+++ minijam189/Scripts/cauldron.gd
@@ -12,5 +12,6 @@ func _on_body_entered(area):
 			$soupTexture.color = area.get_node("InventoryManager").getColor()
 			#get_parent().get_node("GPUParticles2D")
 			$"../GPUParticles2D".modulate = area.get_node("InventoryManager").getColor()
+			get_node("/root/Node2D/Cauldron").Filled(get_node("/root/Node2D/player/Player/InventoryManager").ItemID)
 			area.get_node("InventoryManager").destroyObj()
 		
