1eeec140f175f6435609231cb4cca1e633c5a00d
arrowira <41136800+arrowira@users.noreply.github.com>
2025-07-20 01:29:00 -0700

    cauldron path

--- minijam189/Scripts/PickupObj.gd
+++ minijam189/Scripts/PickupObj.gd
@@ -14,7 +14,7 @@ func _process(delta: float) -> void:
 func _on_body_entered(body):
 	if body.name == "Player":
 		if(get_node("/root/Node2D/player/Player/InventoryManager").Holding == false):
-			if(itemID != get_node("").Item[0]):
+			if(itemID != get_node("/root/Node2D/Cauldron").items[0]):
 				return
 			match itemID:
 				0:
